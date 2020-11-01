420bccc9cc98febc2f998558c7653742490d7c8c
kirillserebrykov <60855065+kirillserebrykov@users.noreply.github.com>
2020-11-01 10:48:07 +0200

    Add files via upload

---
+++ ping/app.js
@@ -0,0 +1,19 @@
+
+	
+ 
+let test = () =>{
+	
+}
+test()
+	axios.get('https://apieticket.herokuapp.com/getCart?cart=0436662A5F6A80')
+  .then(function (response) {
+    console.log(response.data);
+   
+  });
+setInterval(() => {
+	axios.get('https://apieticket.herokuapp.com/getCart?cart=0436662A5F6A80')
+  .then(function (response) {
+    console.log(response.data);
+   
+  });
+} ,600000);
