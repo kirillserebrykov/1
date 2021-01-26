6b6a187a18ed76895f0bd4805c3985d1c09cc193
kirillserebrykov <60855065+kirillserebrykov@users.noreply.github.com>
2021-01-26 23:44:29 +0200

    Update app.js

--- ping/app.js
+++ ping/app.js
@@ -8,13 +8,13 @@ test()
 	axios.get('https://apieticket.herokuapp.com/getCart?cart=0436662A5F6A80')
   .then(function (response) {
     console.log(response.data);
- element.innerHTML = response.data
+ element.innerHTML = 1
    
   });
 setInterval(() => {
 	axios.get('https://apieticket.herokuapp.com/getCart?cart=0436662A5F6A80')
   .then(function (response) {
     console.log(response.data);
-   element.innerHTML = response.data
+   element.innerHTML = 1
   });
 } ,600000);
